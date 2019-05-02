1f777881ee462f2d0a705838e909ec19518f10de
matejkubala <49711617+matejkubala@users.noreply.github.com>
2019-05-02 16:50:01 +0200

    features added
    ak html, js, css su vsetky v index.html tak to ide , ak su v osobytnych suboroch tak to nefunguje
    vyskusal som prepojenie arduino raspberry, nahodne generovane cisla z arduina ktore sa zobrazuju do polohy tachometra
    spatne poslielanie z raspberry do arduina treba doriesit, ma tam casovy vypadok kym vsetko spracuje a neviem ako to posiela, neotestoval som spatne v arduine

--- flask_default/app.py
+++ flask_default/app.py
@@ -13,7 +13,7 @@ ser = serial.Serial("/dev/ttyACM0", 115200)
 async_mode = None
 
 app = Flask(__name__)
-
+    
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
 thread = None
@@ -21,41 +21,44 @@ thread_lock = Lock()
 
 
 LED = 'OFF'
-state = 'stop'
+LED_mode = 'Manual'
+state = 'Stop'
+cruise_state = 'OFF'
+lane_state = 'OFF'
 
 def background_thread(args):
     count = 0
     dataList = []
     while True:
 		socketio.sleep(0.1)
-		if state=='start':
-#                        read_ser = ser.readline()
-#                        print (read_ser)
-#                        socketio.emit('my_response',
-#						  {'data': read_ser, 'count': count},
-#						  namespace='/test')
-			if args:
-			  A = dict(args).get('A')
-			  btnV = dict(args).get('btn_value')
-			else:
-			  A = 1
-			  btnV = 'null' 
-			print A
-			print args  
-			socketio.sleep(1)
-			count += 1
-			prem = random.random()
-			dataDict = {
-			  "t": time.time(),
-			  "x": count,
-			  "y": float(A)*prem}
-			dataList.append(dataDict)
-			if len(dataList)>0:
+		if state=='Start':
+                        read_ser = ser.readline()
+                        print (read_ser)
+                        socketio.emit('new_speed',
+						  {'data': read_ser, 'count': count},
+						  namespace='/test')
+#			if args:
+#			  A = dict(args).get('A')
+#			  btnV = dict(args).get('btn_value')
+#			else:
+#			  A = 1
+#			  btnV = 'null' 
+#			print A
+#			print args  
+#			socketio.sleep(1)
+#			count += 1
+#			prem = random.random()
+#			dataDict = {
+#			  "t": time.time(),
+#			  "x": count,
+#			  "y": float(A)*prem}
+#			dataList.append(dataDict)
+#			if len(dataList)>0:
 #			  print str(dataList)
 #			  print str(dataList).replace("'", "\"")      
-                          socketio.emit('my_response',
-						  {'data': float(A)*math.sin(time.time()), 'count': count},
-						  namespace='/test')
+#                          socketio.emit('my_response',
+#						  {'data': float(A)*math.sin(time.time()), 'count': count},
+#						  namespace='/test')
 
 @app.route('/')
 def index():
@@ -67,43 +70,92 @@ def test_message(message):
     session['A'] = message['value']    
     emit('my_response',
          {'data': message['value'], 'count': session['receive_count']})
- 
-@socketio.on('disconnect_request', namespace='/test')
-def disconnect_request():
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'Disconnected!', 'count': session['receive_count']})
-    disconnect()  
-	
-@socketio.on('e_state', namespace='/test')
-def test_message(message):
-	global state
-	if state == 'stop':
-            state = 'start'
-            global thread
-            with thread_lock:
-                if thread is None:
-                    thread = socketio.start_background_task(target=background_thread, args=session._get_current_object())
-            emit('state_connected', {'data': state, 'count': 0})
-            print 'startujeeeeeeeeeeeeem'
-        else:
-            state ='stop'
-            emit('state_connected',
-                 {'data': state, 'count': 0})
-            print 'konieeeeeeeeeeeeec'
 
 @socketio.on('svetla', namespace='/test')
 def test_message(message):
 	global LED
 	global state
-	if state == 'start':
+	if state == 'Start':
             if LED == 'OFF':
                 LED='ON'
+                emit('ovladanie_LED',
+                         {'data': 'OFF' })
+                ser.write('svetla_ON')
             else:
                 LED='OFF'
-            emit('ovladanie_LED',
-                     {'data': LED })
+                emit('ovladanie_LED',
+                         {'data': 'ON' })
+                ser.write('svetla_OFF')
             print LED
+
+@socketio.on('svetla_auto', namespace='/test')
+def test_message(message):
+	global LED_mode
+	global state
+	if state == 'Start':
+            if LED_mode == 'Auto':
+                LED_mode='Manual'
+                emit('auto_LED',
+                         {'data': 'Auto' })
+            else:
+                LED_mode='Auto'
+                emit('auto_LED',
+                         {'data': 'Manual' })
+            print LED_mode
+
+@socketio.on('lane_request', namespace='/test')
+def test_message(message):
+	global state
+	global lane_state
+	if state == 'Start':
+            if lane_state == 'OFF':
+                lane_state = 'ON'
+                emit('lane_response',
+                         {'data': 'OFF' })
+            else:
+                lane_state = 'OFF'
+                emit('lane_response',
+                         {'data': 'ON' })
+            print lane_state
+
+@socketio.on('cruise_request', namespace='/test')
+def test_message(message):
+	global state
+	global cruise_state
+	if state == 'Start':
+            if cruise_state == 'OFF':
+                cruise_state = 'ON'
+                emit('cruise_response',
+                         {'data': 'OFF' })
+            else:
+                cruise_state = 'OFF'
+                emit('cruise_response',
+                         {'data': 'ON' })
+            print cruise_state
+
+@socketio.on('e_state', namespace='/test')
+def test_message(message):
+	global state
+	if state == 'Stop':
+            state = 'Start'
+            global thread
+            with thread_lock:
+                if thread is None:
+                    thread = socketio.start_background_task(target=background_thread, args=session._get_current_object())
+            emit('state_connected', {'data': 'Stop', 'count': 0})
+            print 'startujeeeeeeeeeeeeem'
+        else:
+            state ='Stop'
+            emit('state_connected',
+                 {'data': 'Start', 'count': 0})
+            print 'konieeeeeeeeeeeeec'
+
+@socketio.on('disconnect_request', namespace='/test')
+def disconnect_request():
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': 'Disconnected!', 'count': session['receive_count']})
+    disconnect()
 
 @socketio.on('speed_input', namespace='/test')
 def test_message(message):
